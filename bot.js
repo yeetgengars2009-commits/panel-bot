df27b4a3fd796ee77096b312bf3c80cced24c6aa
yeetgengars2009-commits <yeetgengars2009@gmail.com>
2026-04-24 16:11:49 +0100

    Refactor bot.js to use service key for Supabase

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ const APP_URL = "https://panel-bot-production.up.railway.app";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_SERVICE_KEY
 );
 
 const app = express();
